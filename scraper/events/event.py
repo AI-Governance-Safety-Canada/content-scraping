586b0d210288909461335258e9ac9036f0274a38
Robert Gambee <robertgambee@gmail.com>
2024-09-08 16:10:38 -0400

    Use DateAndTime for event start and end

--- scraper/events/event.py
+++ scraper/events/event.py
@@ -2,12 +2,14 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional
 
+from scraper.common.types.date_and_time import DateAndTime
+
 
 @dataclass(frozen=True)
 class Event:
     title: Optional[str]
-    start: Optional[datetime]
-    end: Optional[datetime]
+    start: Optional[DateAndTime]
+    end: Optional[DateAndTime]
     description: Optional[str]
     url: Optional[str]
     virtual: Optional[bool]
