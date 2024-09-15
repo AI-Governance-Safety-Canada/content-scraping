70a92bffe084997b0f7c791b2d636982dde53892
Robert Gambee <robertgambee@gmail.com>
2024-09-15 13:34:57 -0400

    Use class to abstract away API details
    I expect this will make it simpler to swap in different APIs in the
    future.

--- scraper/__main__.py
+++ scraper/__main__.py
@@ -9,21 +9,20 @@ from scraper.events.event import Event
 from scraper.events.prompt import EVENT_METHOD, EVENT_PROMPT
 from scraper.events.sources import EVENT_SOURCES
 from scraper.events.parser import parse_full_response
-from scraper.common.api.instant_api import stringify_prompt, submit
+from scraper.common.api.instant_api import InstantAPI
 from scraper.common.writers.format_selector import SUPPORTED_FORMATS, write_items
 
 
 def fetch_events(api_key: str) -> Iterable[Event]:
     logger = logging.getLogger(__name__)
-    prompt = stringify_prompt(EVENT_PROMPT)
+    api = InstantAPI(
+        api_key=api_key,
+        prompt=EVENT_PROMPT,
+        method_name=EVENT_METHOD,
+    )
     for source in EVENT_SOURCES:
         logger.info("Scraping events from %s", source)
-        response = submit(
-            url_to_scrape=source,
-            method_name=EVENT_METHOD,
-            prompt=prompt,
-            api_key=api_key,
-        )
+        response = api.scrape(source)
         if not response:
             continue
         for event in parse_full_response(
--- scraper/common/api/instant_api.py
+++ scraper/common/api/instant_api.py
@@ -1,10 +1,11 @@
 import json
 import logging
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict
 
 import requests
 
 from scraper.common.text_processors.html import convert_html_entities
+from .interface import API, ApiResponse
 
 
 def stringify_prompt(prompt: Dict[str, Any]) -> str:
@@ -16,42 +17,54 @@ def stringify_prompt(prompt: Dict[str, Any]) -> str:
     return json.dumps(json.dumps(prompt, separators=(",", ":")))
 
 
-def submit(
-    url_to_scrape: str,
-    method_name: str,
-    prompt: str,
-    api_key: str,
-) -> Optional[Dict[str, List[Dict[Any, Any]]]]:
-    """Submit a request to InstantAPI and return its response
+class InstantAPI(API):
+    """Class for scraping webpages using InstantAPI
 
     The API documentation lives here:
         https://instantapi.ai/docs/retrieve/api-endpoint/
     """
-    logger = logging.getLogger(__name__)
-    endpoint = "https://instantapi.ai/api/retrieve/"
-    payload = {
-        "webpage_url": url_to_scrape,
-        "api_method_name": method_name,
-        "api_response_structure": prompt,
-        "api_key": api_key,
-    }
-    headers = {"Content-Type": "application/json"}
-    try:
-        response = requests.post(endpoint, json=payload, headers=headers)
-    except requests.RequestException:
-        logger.exception("Failed to get response from %r", endpoint)
-        return None
-    if not response.ok:
-        logger.error(
-            "Request returned response status %d: %s - %s",
-            response.status_code,
-            response.reason,
-            response.text,
-        )
-        return None
-    try:
-        response_json = response.json()
-    except requests.exceptions.JSONDecodeError:
-        logger.exception("Failed to parse response: %r", response.text)
-        return None
-    return convert_html_entities(response_json)
+
+    ENDPOINT = "https://instantapi.ai/api/retrieve/"
+
+    def __init__(
+        self,
+        api_key: str,
+        prompt: Dict[str, Any],
+        method_name: str,
+    ) -> None:
+        self.api_key = api_key
+        self.prompt = stringify_prompt(prompt)
+        self.method_name = method_name
+
+    def scrape(self, url: str) -> ApiResponse:
+        logger = logging.getLogger(__name__)
+        payload = {
+            "webpage_url": url,
+            "api_method_name": self.method_name,
+            "api_response_structure": self.prompt,
+            "api_key": self.api_key,
+        }
+        headers = {"Content-Type": "application/json"}
+        try:
+            response = requests.post(
+                self.ENDPOINT,
+                json=payload,
+                headers=headers,
+            )
+        except requests.RequestException:
+            logger.exception("Failed to get response from %r", self.ENDPOINT)
+            return None
+        if not response.ok:
+            logger.error(
+                "Request returned response status %d: %s - %s",
+                response.status_code,
+                response.reason,
+                response.text,
+            )
+            return None
+        try:
+            response_json = response.json()
+        except requests.exceptions.JSONDecodeError:
+            logger.exception("Failed to parse response: %r", response.text)
+            return None
+        return convert_html_entities(response_json)
