fc4130ba916f9a863753466738b568ec67c4852f
Robert Gambee <robertgambee@gmail.com>
2024-09-15 16:08:01 -0400

    Fetch additional details from event's own page

--- events/pipeline.py
+++ events/pipeline.py
@@ -8,9 +8,7 @@ from .sources import EVENT_SOURCES
 from .parser import parse_full_response
 
 
-def fetch_events(
-    api: API,
-) -> Iterable[Event]:
+def fetch_events(api: API) -> Iterable[Event]:
     logger = logging.getLogger(__name__)
     for source in EVENT_SOURCES:
         logger.info("Scraping events from %s", source)
@@ -23,5 +21,32 @@ def fetch_events(
             scrape_datetime=datetime.now().astimezone(tz=None),
         ):
             logger.debug("%r", event)
-            if event:
-                yield event
+            if not event:
+                continue
+            yield fetch_event_details(api, event)
+
+
+def fetch_event_details(api: API, event: Event) -> Event:
+    """Try to fill in additional details by scraping the event's own page"""
+    logger = logging.getLogger(__name__)
+
+    if not event.url:
+        return event
+
+    logger.info("Fetching details for event: %s", event.url)
+    response = api.scrape(event.url)
+    if not response:
+        return event
+
+    additional_details = parse_full_response(
+        response=response,
+        scrape_source=event.scrape_source,
+        scrape_datetime=event.scrape_datetime,
+    )
+    for detail in additional_details:
+        # Prefer info from the event's own page
+        event = detail.merge(event)
+        # Only use the first event found on the details page
+        break
+
+    return event
