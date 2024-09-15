3f2269ec418c877dac8c0b58f432fe478738efee
Robert Gambee <robertgambee@gmail.com>
2024-09-15 14:37:59 -0400

    Optionally filter out old events

--- scraper/__main__.py
+++ scraper/__main__.py
@@ -7,7 +7,12 @@ from pathlib import Path
 from scraper.events.pipeline import fetch_events
 from scraper.events.prompt import EVENT_METHOD, EVENT_PROMPT
 from scraper.common.api.instant_api import InstantAPI
+from scraper.common.filters.date_and_time import exclude_old_items
 from scraper.common.writers.format_selector import SUPPORTED_FORMATS, write_items
+
+
+# Datetime which is earlier than any legitimate ones we expect to encounter
+EPOCH_START = datetime.date.fromtimestamp(0)
 
 
 def main() -> None:
@@ -27,6 +32,21 @@ def main() -> None:
             Supported file types are {output_formats}.
         """,
     )
+    parser.add_argument(
+        "--after",
+        type=datetime.date.fromisoformat,
+        nargs="?",
+        # If option is not specified, use a very old cutoff to include all events
+        default=EPOCH_START,
+        # If option is specified but no cutoff date is given, use None, which will be
+        # replaced with the current date.
+        const=None,
+        help="""
+            Only include events after this date. If this option is specified without a
+            value, use the current date as the cutoff. If this option is not specified,
+            do not filter by date. Format date according to ISO-8601: 2001-01-31.
+        """,
+    )
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.DEBUG)
@@ -37,6 +57,11 @@ def main() -> None:
         method_name=EVENT_METHOD,
     )
     events = fetch_events(api)
+    events = exclude_old_items(
+        events,
+        cutoff=args.after,
+        key=lambda event: event.start_date or EPOCH_START,
+    )
     write_items(
         items=events,
         output_path=args.output_path,
