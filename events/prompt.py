27b197b85fa55dd280f075519ab5a1cccf1b7124
Robert Gambee <robertgambee@gmail.com>
2024-09-15 17:00:51 -0400

    Adjust prompt to emphasize UTC offset for times

--- events/prompt.py
+++ events/prompt.py
@@ -5,9 +5,9 @@ EVENT_PROMPT = {
         {
             "event_name": "<The name of the event>",
             "start_date": "<The date the event starts, excluding the time, in ISO-8601 format. If the date is not provided, this field is the empty string.>",
-            "start_time": "<The time the event starts in ISO-8601 format including the UTC offset. If the time is not provided, this field is the empty string.>",
+            "start_time": "<The time the event starts, if available. Must be ISO-8601 format and include UTC offset. If the time is not provided, this field is the empty string.>",
             "end_date": "<The date the event ends, excluding the time, in ISO-8601 format. If the date is not provided, this field is the empty string.>",
-            "end_time": "<The time the event ends in ISO-8601 format including the UTC offset. If the time is not provided, this field is the empty string.>",
+            "end_time": "<The time the event ends, if available. Must be in ISO-8601 format and include the UTC offset. If the time is not provided, this field is the empty string.>",
             "event_description": "<A short description of the event in one to three sentences if one is present. Otherwise, the empty string.>",
             "event_url": "<The full URL for the event. If no URL is provided, this field is the empty string.>",
             "event_attendence": "<How participants will join the event: either 'in-person', 'virtual' or 'hybrid'>",
